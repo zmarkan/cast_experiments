5db2ab4a41f7f543b78b5fed37270fbcff06803e
zmarkan <zan.markan@mnscorp.net>
2014-02-28 11:47:31 +0000

    added example gallery roller with 5s loop

---
+++ ImageSender/imagesender.js
@@ -0,0 +1,329 @@
+/**
+ * global variables
+ */
+var currentMediaSession = null;
+var currentVolume = 0.5;
+var progressFlag = 1;
+var mediaCurrentTime = 0;
+var session = null;
+var currentMediaId = 0;
+
+var imagesUrls = [
+  'your images go here',
+  'and here',
+  'and here'
+];
+
+
+/**
+ * Call initialization
+ */
+if (!chrome.cast || !chrome.cast.isAvailable) {
+  setTimeout(initializeCastApi, 1000);
+}
+
+/**
+ * initialization
+ */
+function initializeCastApi() {
+  // default app ID to the default media receiver app
+  // optional: you may change it to your own app ID/receiver
+  var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+
+  var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+  var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+    sessionListener,
+    receiverListener);
+
+  chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+};
+
+/**
+ * initialization success callback
+ */
+function onInitSuccess() {
+	console.log("onInitSuccess");
+  appendMessage("init success");
+  document.getElementById("CastStatus").firstElementChild.innerHTML = 'On Init Success!'; 
+  launchApp();
+}
+
+/**
+ * initialization error callback
+ */
+function onError() {
+  console.log("error");
+  appendMessage("error");
+  document.getElementById("CastStatus").firstElementChild.innerHTML = 'Cast Error!'; 
+}
+
+/**
+ * generic success callback
+ */
+function onSuccess(message) {
+	console.log("onSuccess")
+  console.log(message);
+  
+
+}
+
+function playLoop(){
+
+console.log("playloop reached")
+if(currentMediaId >= imagesUrls.length)
+  {
+  	currentMediaId = 0;
+  }
+
+  loadMedia(currentMediaId);
+
+  currentMediaId = currentMediaId + 1;
+
+}
+
+/**
+ * callback on success for stopping app
+ */
+function onStopAppSuccess() {
+  console.log('Session stopped');
+  appendMessage('Session stopped');
+  document.getElementById("casticon").src = 'images/cast_icon_idle.png'; 
+}
+
+/**
+ * session listener during initialization
+ */
+function sessionListener(e) {
+  console.log('New session ID: ' + e.sessionId);
+  appendMessage('New session ID:' + e.sessionId);
+  session = e;
+  if (session.media.length != 0) {
+    appendMessage(
+        'Found ' + session.media.length + ' existing media sessions.');
+    onMediaDiscovered('onRequestSessionSuccess_', session.media[0]);
+  }
+  session.addMediaListener(
+      onMediaDiscovered.bind(this, 'addMediaListener'));
+  session.addUpdateListener(sessionUpdateListener.bind(this));  
+}
+
+/**
+ * session update listener 
+ */
+function sessionUpdateListener(isAlive) {
+  var message = isAlive ? 'Session Updated' : 'Session Removed';
+  message += ': ' + session.sessionId;
+  appendMessage(message);
+  if (!isAlive) {
+    session = null;
+  }
+};
+
+/**
+ * receiver listener during initialization
+ */
+function receiverListener(e) {
+  if( e === 'available' ) {
+    console.log("receiver found");
+    appendMessage("receiver found");
+  }
+  else {
+    console.log("receiver list empty");
+    appendMessage("receiver list empty");
+  }
+}
+
+/**
+ * select a media URL 
+ * @param {string} m An index for media URL
+ */
+function selectMedia(m) {
+  console.log("media selected" + m);
+  appendMessage("media selected" + m);
+  currentMediaURL = mediaURLs[m]; 
+  var playpauseresume = document.getElementById("playpauseresume");
+  document.getElementById('thumb').src = mediaThumbs[m];
+}
+
+/**
+ * launch app and request session
+ */
+function launchApp() {
+  console.log("launching app...");
+  appendMessage("launching app...");
+  chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+}
+
+/**
+ * callback on success for requestSession call  
+ * @param {Object} e A non-null new session.
+ */
+function onRequestSessionSuccess(e) {
+  console.log("session success: " + e.sessionId);
+  appendMessage("session success: " + e.sessionId);
+  session = e;
+
+  document.getElementById("CastStatus").firstElementChild.innerHTML = 'Cast Ready!'; 
+
+  setInterval(playLoop, 5000);
+
+}
+
+/**
+ * callback on launch error
+ */
+function onLaunchError() {
+  console.log("launch error");
+  appendMessage("launch error");
+}
+
+/**
+ * stop app/session
+ */
+function stopApp() {
+  session.stop(onStopAppSuccess, onError);
+}
+
+/**
+ * load media
+ * @param {string} i An index for media
+ */
+function loadMedia(i) {
+  if (!session) {
+    console.log("no session");
+    appendMessage("no session");
+    return;
+  }
+  currentMediaURL = imagesUrls[i];
+  console.log("loading..." + currentMediaURL);
+  appendMessage("loading..." + currentMediaURL);
+  var mediaInfo = new chrome.cast.media.MediaInfo(currentMediaURL);
+  mediaInfo.contentType = 'image/jpeg';
+  var request = new chrome.cast.media.LoadRequest(mediaInfo);
+
+
+  session.loadMedia(request,
+    onMediaDiscovered.bind(this, 'loadMedia'),
+    onMediaError);
+
+}
+
+/**
+ * callback on success for loading media
+ * @param {Object} e A non-null media object
+ */
+function onMediaDiscovered(how, mediaSession) {
+  console.log("new media session ID:" + mediaSession.mediaSessionId);
+  appendMessage("new media session ID:" + mediaSession.mediaSessionId + ' (' + how + ')');
+  currentMediaSession = mediaSession;
+  
+}
+
+/**
+ * callback on media loading error
+ * @param {Object} e A non-null media object
+ */
+function onMediaError(e) {
+  console.log("media error");
+  appendMessage("media error");
+}
+
+/**
+ * callback for media status event
+ * @param {Object} e A non-null media object
+ */
+function onMediaStatusUpdate(isAlive) {
+  
+}
+
+/**
+ * play media
+ */
+function playMedia() {
+  if( !currentMediaSession ) 
+    return;
+
+  var playpauseresume = document.getElementById("playpauseresume");
+  if( playpauseresume.innerHTML == 'Play' ) {
+    currentMediaSession.play(null,
+      mediaCommandSuccessCallback.bind(this,"playing started for " + currentMediaSession.sessionId),
+      onError);
+      
+      //currentMediaSession.addListener(onMediaStatusUpdate);
+      appendMessage("play started");
+  }
+  else {
+    if( playpauseresume.innerHTML == 'Pause' ) {
+      currentMediaSession.pause(null,
+        mediaCommandSuccessCallback.bind(this,"paused " + currentMediaSession.sessionId),
+        onError);
+      appendMessage("paused");
+    }
+    else {
+      if( playpauseresume.innerHTML == 'Resume' ) {
+        currentMediaSession.play(null,
+          mediaCommandSuccessCallback.bind(this,"resumed " + currentMediaSession.sessionId),
+          onError);
+        appendMessage("resumed");
+      }
+    }
+  }
+}
+
+/**
+ * stop media
+ */
+function stopMedia() {
+  if( !currentMediaSession ) 
+    return;
+
+  currentMediaSession.stop(null,
+    mediaCommandSuccessCallback.bind(this,"stopped " + currentMediaSession.sessionId),
+    onError);
+  var playpauseresume = document.getElementById("playpauseresume");
+  appendMessage("media stopped");
+}
+
+/**
+ * set media volume
+ * @param {Number} level A number for volume level
+ * @param {Boolean} mute A true/false for mute/unmute 
+ */
+function setMediaVolume(level, mute) {
+  if( !currentMediaSession ) 
+    return;
+
+  var volume = new chrome.cast.Volume();
+  volume.level = level;
+  currentVolume = volume.level;
+  volume.muted = mute;
+  var request = new chrome.cast.media.VolumeRequest();
+  request.volume = volume;
+  currentMediaSession.setVolume(request,
+    mediaCommandSuccessCallback.bind(this, 'media set-volume done'),
+    onError);
+}
+
+
+/**
+ * callback on success for media commands
+ * @param {string} info A message string
+ * @param {Object} e A non-null media object
+ */
+function mediaCommandSuccessCallback(info) {
+  console.log(info);
+  appendMessage(info);
+}
+
+
+/**
+ * append message to debug message window
+ * @param {string} message A message string
+ */
+function appendMessage(message) {
+  console.log(JSON.stringify(message));
+};
+
+
+
+
